32138140c6aaf39e51888f66534ba96663e1797c
Manuel-Silva-Hub <alexissilva.22ruiz@gmail.com>
2025-10-22 15:17:40 -0500

    update main file

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { ViseModule } from "./vise.module";
+import './instrumentation';
 
 async function bootstrap() {
   const app = await NestFactory.create(ViseModule);

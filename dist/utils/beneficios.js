3d13ab82a9f6bc62153c56d58c90bf884fb1eff5
Oscar_Cardozo <78360032+OscarCardozoDev@users.noreply.github.com>
2025-11-05 19:50:12 +0000

    RESET: Se devolvio al commit 2424da8f3299e28ca16bf63291b45ad8169fb864. Se reinicio para trabajar antes de telemitry.

---
+++ dist/utils/beneficios.js
@@ -0,0 +1,74 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Descuentos {
+    getDescuento(card, payment, countryRecidence, countryPayment, day) {
+        if (card === "Classic") {
+            return 0;
+        }
+        if (card === "Gold") {
+            const allowDays = ["lunes", "martes", "miercoles"];
+            if (allowDays.includes(day) && payment >= 100) {
+                return 0.15;
+            }
+            else {
+                return 0;
+            }
+        }
+        if (card === "Platinum") {
+            const allowDays = ["lunes", "martes", "miercoles"];
+            const notAllowCountries = ["China", "Vietnam", "India", "Irán"];
+            let discount = 0;
+            if (countryRecidence !== countryPayment &&
+                !notAllowCountries.includes(countryRecidence)) {
+                return 0.05;
+            }
+            if (allowDays.includes(day) && payment >= 100) {
+                discount += 0.2;
+            }
+            if (day === "sabado" && payment >= 200) {
+                discount += 0.3;
+            }
+            return discount;
+        }
+        if (card === "Black") {
+            const allowDays = ["lunes", "martes", "miercoles"];
+            const notAllowCountries = ["China", "Vietnam", "India", "Irán"];
+            let discount = 0;
+            if (countryRecidence !== countryPayment &&
+                !notAllowCountries.includes(countryRecidence)) {
+                return 0.05;
+            }
+            if (allowDays.includes(day) && payment >= 100) {
+                discount += 0.25;
+            }
+            if (day === "sabado" && payment >= 200) {
+                discount += 0.35;
+            }
+            return discount;
+        }
+        if (card === "White") {
+            const notAllowCountries = ["China", "Vietnam", "India", "Irán"];
+            const weekDays = [
+                "lunes",
+                "martes",
+                "miercoles",
+                "jueves",
+                "viernes",
+            ];
+            let discount = 0;
+            if (countryRecidence !== countryPayment &&
+                !notAllowCountries.includes(countryRecidence)) {
+                return 0.05;
+            }
+            if (weekDays.includes(day) && payment >= 100) {
+                discount += 0.25;
+            }
+            if ((day === "sabado" || day === "domingo") && payment >= 200) {
+                discount += 0.35;
+            }
+            return discount;
+        }
+    }
+}
+exports.default = Descuentos;
+//# sourceMappingURL=beneficios.js.map
